f00b61a8122e8764a9d277875c5de85493a85c55
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:53:44 +0100

    Delete health_articles directory

--- health_articles/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class HealthArticlesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'health_articles'
-
-    def ready(self):
-        import health_articles.signals
