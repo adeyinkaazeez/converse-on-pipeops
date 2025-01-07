66c3169e736d4c65daab4bdab0dc2913898a1ad8
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:56:00 +0100

    Delete political_articles directory

--- political_articles/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PoliticalArticlesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'political_articles'
-
-    def ready(self):
-        import political_articles.signals
