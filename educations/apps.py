0975e4b85d8bb1bdf16d96af75cf1390076b2ddd
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:51:19 +0100

    Delete educations directory

--- educations/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class EducationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'educations'
-
-    def ready(self):
-        import educations.signals
