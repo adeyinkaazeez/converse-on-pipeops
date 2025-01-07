7b2494c496d508dc388957eafd7f5e4a4713a361
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:54:50 +0100

    Delete lifestyles directory

--- lifestyles/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class LifestylesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lifestyles'
-    def ready(self):
-        import lifestyles.signals
