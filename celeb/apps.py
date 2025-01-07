88ad84ab86ea3743b03bec310e2c9602aa591fbc
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:50:23 +0100

    Delete celeb directory

--- celeb/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class CelebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'celeb'
-    def ready(self):
-        import celeb.signals
