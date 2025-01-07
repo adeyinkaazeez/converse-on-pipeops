d292a825cb1c12b639485d24f6e0488582af4a64
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:53:24 +0100

    Delete happening directory

--- happening/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class HappeningConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'happening'
-
-    def ready(self):
-        import happening.signals
