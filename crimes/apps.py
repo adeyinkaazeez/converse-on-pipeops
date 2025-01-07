cf3e2adbd98317de3922d3d2188236d713a01e33
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:50:44 +0100

    Delete crimes directory

--- crimes/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrimesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crimes'
-
-    def ready(self):
-        import crimes.signals
