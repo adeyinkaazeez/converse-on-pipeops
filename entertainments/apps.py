721888e985356d0477342e69ad8581c405b1d132
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:51:37 +0100

    Delete entertainments directory

--- entertainments/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class EntertainmentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'entertainments'
-
-    def ready(self):
-        import entertainments.signals
