36fae7610dde9e13601a0f7d0c215f73cd93779c
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:55:23 +0100

    Delete newss directory

--- newss/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewssConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'newss'
-    def ready(self):
-        import newss.signals
