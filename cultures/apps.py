85c22711a3b6ec3716a438f3c49fce36ec434f6e
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:51:02 +0100

    Delete cultures directory

--- cultures/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CulturesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cultures'
-
-    def ready(self):
-        import cultures.signals
