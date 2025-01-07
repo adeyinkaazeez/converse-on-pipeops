aeb56c66ad357d5b821e5f25cbf8146962c7374a
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:54:35 +0100

    Delete internationals directory

--- internationals/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class InternationalsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'internationals'
-
-    def ready(self):
-        import internationals.signals
