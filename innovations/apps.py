ca421560ee3b1ee93fe17f73200198a7acbdc3f6
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:54:19 +0100

    Delete innovations directory

--- innovations/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class InnovationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'innovations'
-
-    def ready(self):
-        import innovations.signals
