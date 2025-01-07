bdcb6deffca0f002b5ce5a497e08d1e1387ec9fd
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-07 13:54:02 +0100

    Delete howtos directory

--- howtos/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class HowtosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'howtos'
-
-    def ready(self):
-        import howtos.signals
